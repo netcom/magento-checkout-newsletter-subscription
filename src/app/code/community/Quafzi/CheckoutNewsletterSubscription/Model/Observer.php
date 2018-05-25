996f36d8b6f60d96800aa0b22ed26e276f7aeff2
NetCom <1984432+netcom@users.noreply.github.com>
2018-05-25 16:16:22 +0200

    Works with advancednewsletter

--- src/app/code/community/Quafzi/CheckoutNewsletterSubscription/Model/Observer.php
+++ src/app/code/community/Quafzi/CheckoutNewsletterSubscription/Model/Observer.php
@@ -9,7 +9,7 @@ class Quafzi_CheckoutNewsletterSubscription_Model_Observer
         ) {
             $html = $observer->getTransport()->getHtml();
             $checkboxHtml = '<li><p class="agree">'
-                . '<input id="subscribe_newsletter" name="is_subscribed" checked="checked" value="1" class="checkbox" type="checkbox" />'
+                . '<input id="subscribe_newsletter" name="is_subscribed" value="1" class="checkbox" type="checkbox" />'
                 . '<label for="subscribe_newsletter">' . Mage::helper('sales')->__('Subscribe to Newsletter') . '</label>'
                 . '</p></li>';
             $html = str_replace('</ol>', $checkboxHtml . '</ol>', $html);
@@ -17,13 +17,24 @@ class Quafzi_CheckoutNewsletterSubscription_Model_Observer
         }
     }
 
-    public function subscribe ($observer)
+    public function subscribe($observer)
     {
         $quote = $observer->getEvent()->getQuote();
         if ($quote->getBillingAddress() && Mage::app()->getRequest()->getParam('is_subscribed', false)) {
+			//Mage::log($quote->getBillingAddress()->getEmail(), null, 'checkout_subscribe.log');
+            //works with aw advancednewsletter
+			$subscriber = Mage::getModel('advancednewsletter/subscriber')/*->loadByEmail($_POST['email'])*/;
+			try{
+				$subscriber->subscribe($quote->getBillingAddress()->getEmail(), array('default'),array());
+			} catch( Exception $e)
+			{
+				Mage::logException($e);
+			}
+			/*
             $status = Mage::getModel('newsletter/subscriber')
                 ->setImportMode(true)
                 ->subscribe($quote->getBillingAddress()->getEmail());
+			*/
         }
     }
 
